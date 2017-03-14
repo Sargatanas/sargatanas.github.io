7c95bbbe1e957c00def39e492880ca73356a4373
Silvamorta <kainkaedecross@gmail.com>
2017-03-15 03:22:55 +0500

    Проработана лаб 3

--- js/script.js
+++ js/script.js
@@ -1,117 +1,139 @@
-﻿// Задание 1
-function Ex1() {
-    var n = document.getElementById('n1').value;
-    var count = 0;
-    var string = '';
-    var min;
+﻿"use strict";
+
+// Задание 1
+function taskOne()
+{
+    let n = document.getElementById('input_1').value;
+    let count = 0;
+    let string = '';
+    let min;
 
     //обработаем введенные данные на валидность
-    if (n < 1 || n % 1 != 0) {
+    if ((n < 1) || (n % 1 !== 0)) {
         string = 'не сгенерирована';
         min = '#';
         count = '#';
-    }
-    else
-    {
+    } else {
         //считаем первый элемент
         min = Math.floor(Math.random() * 20);
         string = string + min + ' &#8196;&#8196; ';
 
         //накопим остальные
-        var temp;
-        for (var i = 1; i < n; i++) {
+        let temp;
+        for (let i = 1; i < n; i++) {
             temp = Math.floor(Math.random() * 20);
             string = string + temp + ' &#8196; ';
             if (temp < min) {
                 min = temp;
-                count = 1
+                count = 1;
+            } else if (temp === min){
+                 count++;
             }
-            else
-                if (temp == min) count++;
         }
     }
 
     //выведем результат
-    document.getElementById('output1').innerHTML = string;
-    document.getElementById('min1').innerHTML = String(min);
-    document.getElementById('count1').innerHTML = String(count);
+    document.getElementById('output_1_sequence').innerHTML = string;
+    document.getElementById('output_1_min').innerHTML = String(min);
+    document.getElementById('output_1_count').innerHTML = String(count);
 }
+document.getElementById('button_1').onclick = taskOne;
+
 
 // Задание 2
-function Ex2() {
-    var n = document.getElementById('n2').value;
-    var string = '';
+function taskTwo()
+{
+    let num = document.getElementById('input_2').value;
+    let string = '';
 
     //вычислим сумму цифр "контрольного" числа
-    var cont_sum = 0;
-    for (var n1 = n; n1 > 0; n1 = Math.floor(n1 / 10))
-        cont_sum += n1 % 10;
+    let control_sum = 0;
+    let num_copy;
+    for (num_copy = num; num_copy > 0; num_copy = Math.floor(num_copy / 10))
+        control_sum += num_copy % 10;
 
-    //проверим числа от  1 до n-1
-    var sum;
-    for (var i = 1; i < n; i++) {
+    //проверим числа от 1 до n-1
+    let sum;
+    for (let i = 1; i < num; i++) {
         sum = 0;
-        for (n1 = i; n1 > 0; n1 = Math.floor(n1 / 10))
-            sum += n1 % 10;
-
-        if (sum == cont_sum)
+        for (num_copy = i; num_copy > 0; num_copy = Math.floor(num_copy / 10)) {
+            sum += num_copy % 10;
+        }
+        if (sum == control_sum) {
             string += ' &#8196; ' + i;
+        }
     }
 
-    if (string == '' || n < 1 || n % 1 != 0)
-    {
+    //оценим результат
+    if ((string == '') || (num < 1) || (num % 1 != 0)) {
         string = 'нет';
-        n = '#';
+        num = '#';
     }
 
     //выведем результат
-    document.getElementById('count2').innerHTML = String(n);
-    document.getElementById('output2').innerHTML = string;
+    document.getElementById('output_2_num').innerHTML = String(num);
+    document.getElementById('output_2_sequence').innerHTML = string;
 }
+document.getElementById('button_2').onclick = taskTwo;
+
 
 // Задание 3
-function Ex3() {
-    var data = new Date(document.getElementById('n3').value);
-    var cur_data = new Date();
+function taskThree()
+{
+    let data = new Date(document.getElementById('input_3').value);
+    data.setHours(0);
+    let current_data = new Date();
+    current_data.setHours(0);
+    current_data.setMinutes(0);
+    current_data.setSeconds(0);
 
     //вычислим разницу между датами
-    var dif_date = cur_data.getTime() - data.getTime();
-    dif_date = Math.floor(dif_date/1000/60/60/24) + 1;
+    let dif_date = current_data.getTime() - data.getTime();
+    dif_date = Math.floor(dif_date/1000/60/60/24);
 
     //выведем результат
-    document.getElementById('output3').innerHTML = String(dif_date);
+    if (!data.valid) {
+        dif_date = 0;
+    }
+    document.getElementById('output_3').innerHTML = String(dif_date);
 
-    //поставим в правильный педаеж слово "день"
-    if (dif_date % 10 == 1 && dif_date != 11)
-        document.getElementById('count3').innerHTML = 'день';
-    else
-        if (dif_date % 10 == 2 && dif_date != 12 ||
+    //поставим в правильный падеж слово "день"
+    if ((dif_date % 10 == 1) && (dif_date != 11))
+        document.getElementById('output_3_day').innerHTML = 'день';
+    else if (dif_date % 10 == 2 && dif_date != 12 ||
             dif_date % 10 == 3 && dif_date != 13 ||
-            dif_date % 10 == 4 && dif_date != 14)
-            document.getElementById('count3').innerHTML = 'дня';
-        else
-            document.getElementById('count3').innerHTML = 'дней';
+            dif_date % 10 == 4 && dif_date != 14) {
+        document.getElementById('output_3_day').innerHTML = 'дня';
+    } else {
+        document.getElementById('output_3_day').innerHTML = 'дней';
+    }
 }
+document.getElementById('button_3').onclick = taskThree;
+
 
 // Задание 4
-function Ex4_f() {
+function taskFourFocus() {
     window.status = 'Определитесь с выбором';
     document.getElementById('status_bar').innerHTML = 'Определитесь с выбором';
 }
-function Ex4_b() {
+function taskFourBlur() {
     window.status = '';
     document.getElementById('status_bar').innerHTML = '&#8199;';
 }
+document.getElementById('checkbox_4').onfocus = taskFourFocus;
+document.getElementById('checkbox_4').onblur = taskFourBlur;
+
 
 // К заданию 5 и 6
-function AddCalendar(id, year) {
+function addCalendar(id, year)
+{
     //рассчитаем количество дней в месяце
-    var date = new Date(year, new Date().getMonth(), 1);
-    var month = date.getMonth();
-    var count_days = 33 - new Date(year, date.getMonth(), 33).getDate();
+    let date = new Date(year, new Date().getMonth(), 1);
+    let month = date.getMonth();
+    let count_days = 33 - new Date(year, month, 33).getDate();
 
     //получим название месяца и год
-    var string = '';
+    let string = '';
     switch (month) {
         case 0 : string = 'Январь'; break;
         case 1 : string = 'Февраль'; break;
@@ -129,14 +151,24 @@ function AddCalendar(id, year) {
     string += ' ' + date.getFullYear();
 
     //очистим календарь до шапки
-    document.getElementById(id).innerHTML = '<tr><th colspan="7" class="table-header">' + string + '</th></tr>';
-    string = '<tr><th>пн</th><th>вт</th><th>ср</th><th>чт</th>'; //сюда будем накапливать данные о строке таблицы
-    string += '<th>пт</th><th>сб</th><th>вс</th></tr>';
-    document.getElementById(id).innerHTML += string;
+    document.getElementById(id).innerHTML = `<tr>
+                                                <th colspan="7" class="table-header">${string}</th>
+                                             </tr>
+                                             <tr>
+                                                <th>пн</th>
+                                                <th>вт</th>
+                                                <th>ср</th>
+                                                <th>чт</th>
+                                                <th>пт</th>
+                                                <th>сб</th>
+                                                <th>вс</th>
+                                             </tr>`;
 
     //рассчитаем день недели
-    var day = date.getDay();
-    if (day == 0) day = 7;
+    let day = date.getDay();
+    if (day == 0) {
+        day = 7;
+    }
 
     // заполним первую строку календаря
     string = '<tr>';
@@ -171,190 +203,152 @@ function AddCalendar(id, year) {
         document.getElementById(id).innerHTML += string + '</tr>';
     }
 }
-AddCalendar('cal_1', new Date().getFullYear());
-AddCalendar('cal_2', new Date().getFullYear());
-
-
-function Hidden() {
-    var textarea = document.getElementById('other-field');
-    if(this.checked)
-        textarea.style.display = 'block';
-    else
-        textarea.style.display = 'none'
+function loadCalendar(year) {
+    addCalendar('calendar_1', year);
+    addCalendar('calendar_2', year);
 }
+document.body.onload = loadCalendar.bind({},new Date().getFullYear());
+document.getElementById('button_6_1').onclick = addCalendar.bind({}, "calendar_2", new Date().getFullYear() - 1);
+document.getElementById('button_6_2').onclick = addCalendar.bind({}, "calendar_2", new Date().getFullYear());
+document.getElementById('button_6_3').onclick = addCalendar.bind({}, "calendar_2", new Date().getFullYear() + 1);
 
-document.getElementById('check-4').onchange = Hidden;
 
-function Ex7_1() {
-    var opt = document.getElementsByName('science');
-    if(this.checked)
-        for (var i = 0; i < opt.length; i++)
-        {
-            opt[i].style.display = 'block';
-        }
-
-    else
-        for (var j = 0; j < opt.length; j++)
-        {
-            opt[j].style.display = 'none';
-        }
+// К заданию 7
+function hidden()
+{
+    var area = document.getElementById('other-field');
+    if(this.checked) {
+        area.style.display = 'block';
+    } else {
+        area.style.display = 'none';
+    }
+        
 }
+document.getElementById('check-4').onchange = hidden;
 
-document.getElementById('check-1').onchange = Ex7_1;
-
-function Ex7_2() {
-    var opt = document.getElementsByName('creation');
-    if(this.checked)
-        for (var i = 0; i < opt.length; i++)
-        {
-            opt[i].style.display = 'block';
-        }
-
-    else
-        for (var j = 0; j < opt.length; j++)
-        {
-            opt[j].style.display = 'none';
-        }
+function ex7Public (event)
+{
+    let opt = document.getElementsByName(event.target.dataset.attribute);
+     if(this.checked) {
+         for (let i = 0; i < opt.length; i++) {
+             opt[i].style.display = 'block';
+         }
+     } else {
+         for (let i = 0; i < opt.length; i++) {
+             opt[i].style.display = 'none';
+         }
+     }
+     
 }
+document.getElementById('check-1').onchange = ex7Public;
+document.getElementById('check-2').onchange = ex7Public;
+document.getElementById('check-3').onchange = ex7Public;
 
-document.getElementById('check-2').onchange = Ex7_2;
-
-function Ex7_3() {
-    var opt = document.getElementsByName('sport');
-    if(this.checked)
-        for (var i = 0; i < opt.length; i++)
-        {
-            opt[i].style.display = 'block';
-        }
-
-    else
-        for (var j = 0; j < opt.length; j++)
-        {
-            opt[j].style.display = 'none';
-        }
+function invalidInput(error, field, string)
+{
+    document.getElementById(error).innerHTML = string;
+    document.getElementById(error).style.color = 'red';
+    document.getElementById(field).style.border = '1px solid red';
 }
-
-document.getElementById('check-3').onchange = Ex7_3;
-
-function Er() {
-    var srn = document.getElementById('surname-field').value;
-    var n = document.getElementById('name-field').value;
-    var email = document.getElementById('email-field').value;
-    var city = document.getElementById('select-field').value;
-    var publication = document.getElementById('selector').value;
-    var crit2 = true;
-
-    if (srn == '')
-    {
-        document.getElementById('er1').innerHTML = 'Это поле обязательно для заполнения';
-        document.getElementById('er1').style.color = 'red';
-        document.getElementById('surname-field').style.border = '1px solid red';
-        crit2 = false;
-    }
-    else
-    {
-        var crit = true;
-        var length = srn.length;
-        var char = srn.charCodeAt(0);
-        if (char < 65 || char > 90 && char < 1040 || char >= 1066 && char <=1068 || char > 1071)
-            crit = false;
-        for (var i = 1; i < length; i++)
-        {
-            char = srn.charCodeAt(i);
-            if (char < 97 || char > 122 && char < 1072 || char > 1103)
-                crit = false;
-        }
-        if (!crit)
-        {
-            document.getElementById('er1').innerHTML = 'введите корректную фамилию <br>(с заглавной буквы, без небуквенных символов)';
-            document.getElementById('er1').style.color = 'red';
-            document.getElementById('surname-field').style.border = '1px solid red';
-        }
-        else
-        {
-            document.getElementById('er1').innerHTML = '';
-            document.getElementById('er1').style.color = 'lightgray';
-            document.getElementById('surname-field').style.border = '1px solid gray';
-        }
-    }
-
-    if (n == '')
-    {
-        document.getElementById('er2').innerHTML = 'Это поле обязательно для заполнения';
-        document.getElementById('er2').style.color = 'red';
-        document.getElementById('name-field').style.border = '1px solid red';
-        crit2 = false;
-    }
-    else
-    {   var length1 = n.length;
-        var crit1 = true;
-        var char1 = n.charCodeAt(0);
-        if (char1 < 65 || char1 > 90 && char1 < 1040 || char1 >= 1066 && char1 <=1068 || char1 > 1071)
-            crit1 = false;
-        for (var j = 1; j < length; j++)
-        {
-            char1 = n.charCodeAt(j);
-            if (char1 < 97 || char1 > 122 && char1 < 1072 || char1 > 1103)
-                crit1 = false;
-        }
-        if (!crit1)
-        {
-            document.getElementById('er2').innerHTML = 'введите корректное имя <br>(с заглавной буквы, без небуквенных символов)';
-            document.getElementById('er2').style.color = 'red';
-            document.getElementById('name-field').style.border = '1px solid red';
-            crit2 = false;
-        }
-        else
-        {
-            document.getElementById('er2').innerHTML = '';
-            document.getElementById('er2').style.color = 'lightgray';
-            document.getElementById('name-field').style.border = '1px solid gray';
-        }
-    }
-
-    if (email == '')
-    {
-        document.getElementById('er3').innerHTML = 'Это поле обязательно для заполнения';
-        document.getElementById('er3').style.color = 'red';
-        document.getElementById('email-field').style.border = '1px solid red';
-        crit2 = false;
-    }
-    else
-    {
-        document.getElementById('er3').innerHTML = '';
-        document.getElementById('er3').style.color = 'lightgray';
-        document.getElementById('email-field').style.border = '1px solid gray';
-    }
-
-    if (city == '-выберите город-')
-    {
-        document.getElementById('er5').innerHTML = 'Выберите, пожалуйста, ваш город';
-        document.getElementById('er5').style.color = 'red';
-        document.getElementById('select-field').style.border = '1px solid red';
-        crit2 = false;
-    }
-    else
-    {
-        document.getElementById('er5').innerHTML = '';
-        document.getElementById('er5').style.color = 'lightgray';
-        document.getElementById('select-field').style.border = '1px solid gray';
-    }
-
-    if (publication == '')
-    {
-        document.getElementById('er6').innerHTML = 'Выберите, пожалуйста, журнал или газету для подписки';
-        document.getElementById('er6').style.color = 'red';
-        document.getElementById('selector').style.border = '1px solid red';
-        crit2 = false;
-    }
-    else
-    {
-        document.getElementById('er6').innerHTML = '';
-        document.getElementById('er6').style.color = 'lightgray';
-        document.getElementById('selector').style.border = '1px solid gray';
-    }
-
-    if (crit && crit1 && crit2)
-        form.submit();
+function validInput(error, field)
+{
+    document.getElementById(error).innerHTML = '&#8199;';
+    document.getElementById(error).style.color = 'lightgray';
+    document.getElementById(field).style.border = '1px solid gray';
 }
+function validateName(error, value, string)
+{
+    let criterion = true;
+    let length = value.length;
+    let char = value.charAt(0);
+    if ((char < 'A') || (char > "Z") && (char < 'А') || (char >= 'Ъ') && (char <='Ь') || (char > 'Я')) {
+        criterion = false;
+    }
+    for (let i = 1; i < length; i++) {
+        char = value[i];
+        if ((char < 'a') || (char > 'z') && (char < 'а') || (char > 'я')) {
+            criterion = false;
+        }
+    }
+    if (!criterion) {
+        invalidInput(error, string, "введите корректные данные <br>(с заглавной буквы, без небуквенных символов)");
+    } else {
+        validInput(error, string);
+    }
+    return criterion;
+}
+function addValue(string, field)
+{
+    if (document.getElementById(field).value !== '') {
+        document.getElementById('re-form').innerHTML += '<p>' + string + document.getElementById(field).value + '</p>';
+    }
+}
+function error()
+{
+    let surname = document.getElementById('surname-field').value;
+    let name = document.getElementById('name-field').value;
+    let email = document.getElementById('email-field').value;
+    let city = document.getElementById('city-field').value;
+    let publication = document.getElementById('selector').value;
+    let criterion_0 = true; //критерий, отвечающий за непустоту ключевых полей
+                            // не используем required - атрибут обязательного для заполнения поля
+    let criterion_1 = true;
+    let criterion_2 = true;
 
+    if (surname === '') {
+        invalidInput("error_7_1", "surname-field", "это поле обязательно для заполнения");
+        criterion_0 = false;
+    } else {
+        criterion_1 =  validateName("error_7_1", surname, "surname-field");
+    }
+
+    if (name === '') {
+        invalidInput("error_7_2", "name-field", "это поле обязательно для заполнения");
+        criterion_0 = false;
+    } else {
+        criterion_2 =  validateName("error_7_2", name, "name-field");
+    }
+
+    if (email === '') {
+        invalidInput("error_7_3", "email-field", "это поле обязательно для заполнения");
+        criterion_0 = false;
+    } else {
+        validInput("error_7_3", "email-field");
+    }
+
+    if (city === '-выберите город-') {
+        invalidInput("error_7_4", "city-field", "пожалуйста, выберите город");
+        criterion_0 = false;
+    } else {
+        validInput("error_7_4", "city-field");
+    }
+
+    if (publication === '') {
+        invalidInput("error_7_5", "selector", "пожалуйста, выберите журнал или газету для подписки");
+        criterion_0 = false;
+    } else {
+        validInput("error_7_5", "selector");
+    }
+    if (criterion_0 && criterion_1 && criterion_2) {
+        document.getElementById('re-form').style.display = 'block';
+        document.getElementById('re-form').innerHTML = '<h2>Ваши данные успешно переданы!</h2>';
+        addValue("Фамилия: ", "surname-field");
+        addValue("Имя: ", "name-field");
+        document.getElementById('re-form').innerHTML += '<hr>';
+        addValue("Электронная почта: ", "email-field");
+        addValue("Телефон: ", "phone-field");
+        addValue("Город: ", "city-field");
+        document.getElementById('re-form').innerHTML += '<hr>';
+        document.getElementById('re-form').innerHTML += '<p>Выбранные публикации:</p>';
+        document.getElementById('re-form').innerHTML += '<ul>';
+        let opt = document.getElementsByClassName('options');
+        for (let i = 0; i < opt.length; i++) {
+            if (opt[i].selected) {
+                document.getElementById('re-form').innerHTML += '<li>' + opt[i].label + '</li>';
+            }
+        }
+        document.getElementById('re-form').innerHTML += '</ul>';
+        document.getElementById('re-form').innerHTML += '<hr>';
+        document.getElementById('re-form').innerHTML += '<p class="thanks">Спасибо за заказ!</p>';
+    }
+}
