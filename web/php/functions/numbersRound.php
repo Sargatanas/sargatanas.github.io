58e1dfc4379ae27c0d23466f9b7c93f26b352aba
Silvamorta <kainkaedecross@gmail.com>
2017-03-23 02:28:03 +0500

    исправлено задание 3

--- web/php/functions/numbersRound.php
+++ web/php/functions/numbersRound.php
@@ -3,22 +3,19 @@
 $n = $_GET['n'];
 $k = $_GET['k'];
 
-$string = '';
-$temp = round(1 / $n, $k);
-$temp_max = round(1 / $n, $k + 1);;
-
-if ($temp - $temp_max <= 0) {
-    $string .= $temp;
-} else {
-    $temp -= pow(0.1, $k);
-    $string .= $temp;
-}
-
-$length = strlen($string);
-$real_length = $k + 2;
-if ($length < $real_length) {
-    for ($i = 0; $i < ($real_length - $length); $i++) {
-        $string .= '0';
+$string = '0.';
+$temp = 10;
+for ($i = 0; $i < $k;) {
+    for (;$temp < $n;) {
+        $temp *= 10;
+        $string .= "0";
+        $i++;
+    }
+    if ($i < $k) {
+        $string .= floor($temp / $n);
+        $temp %= $n;
+        $temp *= 10;
+        $i++;
     }
 }
 
